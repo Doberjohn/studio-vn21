8839986d437e9d88072be0f3f741643b24de9688
johnn <johnf@risk.nl>
2022-11-16 03:00:58 +0200

    added responsiveness rules

--- src/components/templates/ReaderTemplate/ReaderTemplate.tsx
+++ src/components/templates/ReaderTemplate/ReaderTemplate.tsx
@@ -25,7 +25,7 @@ export const ReaderTemplate = ({storyId}: ReaderTemplateProps) => {
                className="px-0"/>
          </Div>
          <Div className="row">
-            <Div className="col-lg-12" style={{fontSize: 18}}>
+            <Div className="col-md-7 px-4" style={{fontSize: 18}}>
                <Paragraph className="text-justify">{`They all look so happy. And why wouldn’t they be? It’s the happiest day of their life and one more “worst day” of mine. After all these years, I have gotten used to asking them to smile at me. Though, I haven’t been able to smile myself behind the camera.`}</Paragraph>
                <Paragraph className="text-justify">{`I am trapped in a job I used to love, and now it makes me sick. Photography was my life and passion, but I traded my imagination for a salary. Thus, weddings had my creativity run dry.`}</Paragraph>
                <Paragraph className="text-justify">{`I fear the day my love for my art will cease to exist.`}</Paragraph>
