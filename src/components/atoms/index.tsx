d93b7b68b43406db97fcd3c1c00355f3939611bf
johnn <johnf@risk.nl>
2022-12-06 19:18:38 +0200

    Version 0.7.5@alpha  - Refactored code

--- src/components/atoms/index.tsx
+++ src/components/atoms/index.tsx
@@ -3,11 +3,5 @@ import { Image } from './Image';
 import { LoadingSpinner } from './LoadingSpinner';
 import { Paragraph } from './Paragraph';
 import { Span } from './Span';
-import {
-   ButtonsBox, Loop, Next, PageTemplate, Pause, Play, PlayerTemplate, Previous, Shuffle, Time,
-   Title } from './AudioPlayer';
 
-export {
-   Div, Span, Paragraph, Image, LoadingSpinner, ButtonsBox, Loop, Next, PageTemplate, Pause, Play,
-   PlayerTemplate, Previous, Shuffle, Time, Title
-};
+export { Div, Span, Paragraph, Image, LoadingSpinner };
---
+++ src/components/organisms/VOPlayer/components/index.tsx
@@ -0,0 +1,7 @@
+import { ButtonsBox, Loop, Next, PageTemplate, Pause, Play, PlayerTemplate, Previous, Shuffle, Time,
+   Title } from './atoms';
+import { Progress, Volume } from './molecules';
+
+export {
+   ButtonsBox, Loop, Next, PageTemplate, Pause, Play, PlayerTemplate, Previous, Progress,
+   Shuffle, Time, Title, Volume };
